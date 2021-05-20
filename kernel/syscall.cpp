a4007621d1f876c430bd624f80cc3b1a60004cbd
yutaroyamanaka <falcon4842@gmail.com>
2021-05-20 19:48:57 +0900

    day22c add timer

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -8,6 +8,7 @@
 #include "task.hpp"
 #include "terminal.hpp"
 #include "font.hpp"
+#include "timer.hpp"
 
 namespace syscall {
   struct Result {
@@ -112,18 +113,25 @@ namespace syscall {
         return Result{0, 0};
         }, arg1, arg2, arg3, arg4, arg5, arg6);
   }
+
+  SYSCALL(GetCurrentTick) {
+    return {
+      time_manager->CurrentTick(), kTimerFreq
+    };
+  }
 #undef SYSCALL
 }
 
 
 using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-extern "C" std::array<SyscallFuncType*, 6> syscall_table {
+extern "C" std::array<SyscallFuncType*, 7> syscall_table {
     /* 0x00 */ syscall::LogString,
     /* 0x01 */ syscall::PutString,
     /* 0x02 */ syscall::Exit,
     /* 0x03 */ syscall::OpenWindow,
     /* 0x04 */ syscall::WinWriteString,
     /* 0x05 */ syscall::WinFillRectangle,
+    /* 0x06 */ syscall::GetCurrentTick,
 };
 
 void InitializeSyscall() {
