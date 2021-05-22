0c42b3a4355d7e90ce0a97f7b3e9a7799e6a6a38
yutaroyamanaka <falcon4842@gmail.com>
2021-05-22 23:37:57 +0900

    day23a

--- kernel/app_event.hpp
+++ kernel/app_event.hpp
@@ -1,13 +1,25 @@
-#pragma once
-
 #ifdef __cplusplus
+#include <cstdint>
+#include <cstddef>
 extern "C" {
+#else
+#include <stddef.h>
+#include <stdint.h>
 #endif
 
 struct AppEvent {
   enum Type {
     kQuit,
+    kMouseMove,
   } type;
+
+  union {
+    struct {
+      int x, y;
+      int dx, dy;
+      uint8_t buttons;
+    } mouse_move;
+  } arg;
 };
 
 #ifdef __cplusplus
