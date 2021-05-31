d5169099c4fdf2fc2cddd4adacd04951abc905d0
yutaroyamanaka <falcon4842@gmail.com>
2021-06-01 00:08:36 +0900

    day26c cp command

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -310,6 +310,16 @@ namespace syscall {
       task.Files().emplace_back();
       return num_files;
     }
+
+    std::pair<fat::DirectoryEntry*, int> CreateFile(const char* path) {
+      auto [file, err] = fat::CreateFile(path);
+      switch (err.Cause()) {
+        case Error::kIsDirectory: return { file, EISDIR };
+        case Error::kNoSuchEntry: return { file, ENOENT };
+        case Error::kNoEnoughMemory: return { file, ENOSPC };
+        default: return { file, 0 };
+      }
+    }
   }
 
   SYSCALL(OpenFile) {
@@ -323,19 +333,22 @@ namespace syscall {
       return {0, 0};
     }
 
-    if((flags & O_ACCMODE) == O_WRONLY) {
-      return {0, EINVAL};
-    }
-
-    auto [dir, post_slash] = fat::FindFile(path);
-    if(dir == nullptr) {
-      return {0, ENOENT};
-    } else if(dir->attr != fat::Attribute::kDirectory && post_slash) {
+    auto [file, post_slash] = fat::FindFile(path);
+    if(file == nullptr) {
+      if((flags & O_CREAT) == 0) {
+        return {0, ENOENT};
+      }
+      auto [new_file, err] = CreateFile(path);
+      if(err) {
+        return {0, err};
+      }
+      file = new_file;
+    } else if(file->attr != fat::Attribute::kDirectory && post_slash) {
       return {0, ENOENT};
     }
 
     size_t fd = AllocateFD(task);
-    task.Files()[fd] = std::make_unique<fat::FileDescriptor>(*dir);
+    task.Files()[fd] = std::make_unique<fat::FileDescriptor>(*file);
     return {fd, 0};
   }
 
