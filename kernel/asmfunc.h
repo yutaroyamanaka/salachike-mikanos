c22503b379011b9b394fd1e32cd9a19011e6e0e4
yutaroyamanaka <falcon4842@gmail.com>
2021-06-03 22:59:56 +0900

    day27d copy on write

--- kernel/asmfunc.h
+++ kernel/asmfunc.h
@@ -9,7 +9,9 @@ extern "C" {
   void LoadGDT(uint16_t limit, uint64_t offset);
   void SetCSSS(uint16_t cs, uint16_t ss);
   void SetDSAll(uint16_t value);
+  void SetCR0(uint64_t value);
   void SetCR3(uint64_t value);
+  uint64_t GetCR0();
   uint64_t GetCR2();
   uint64_t GetCR3();
   void SwitchContext(void* next_ctx, void* current_ctx);
@@ -20,4 +22,5 @@ extern "C" {
   void WriteMSR(uint32_t msr, uint64_t value);
   void SyscallEntry(void);
   void ExitApp(uint64_t rsp, int32_t ret_val);
+  void InvalidateTLB(uint64_t addr);
 }
--- kernel/paging.cpp
+++ kernel/paging.cpp
@@ -3,6 +3,7 @@
 #include "asmfunc.h"
 #include "memory_manager.hpp"
 #include "task.hpp"
+#include "logger.hpp"
 
 namespace {
   const uint64_t kPageSize4K = 4096;
@@ -24,7 +25,8 @@ void SetupIdentityPageTable() {
     }
   }
 
-  SetCR3(reinterpret_cast<uint64_t>(&pml4_table[0]));
+  ResetCR3();
+  SetCR0(GetCR0() & 0xfffeffff);
 }
 
 void InitializePaging() {
@@ -54,8 +56,7 @@ namespace {
   }
 
   WithError<size_t> SetupPageMap(
-      PageMapEntry* page_map, int page_map_level, LinearAddress4Level addr, size_t num_4kpages
-      ) {
+      PageMapEntry* page_map, int page_map_level, LinearAddress4Level addr, size_t num_4kpages, bool writable) {
     while(num_4kpages > 0) {
       const auto entry_index = addr.Part(page_map_level);
       auto [child_map, err] = SetNewPageMapIfNotPresent(page_map[entry_index]);
@@ -64,14 +65,15 @@ namespace {
         return { num_4kpages, err };
       }
 
-      page_map[entry_index].bits.writable = 1; /* allow write in vaddr */
       page_map[entry_index].bits.user = 1; /* allow memory accsss regardless of CPL values */
 
       if(page_map_level == 1) {
+        page_map[entry_index].bits.writable = writable;
         num_4kpages--;
       } else {
+        page_map[entry_index].bits.writable = true;
         auto [ num_remain_pages, err ] = 
-          SetupPageMap(child_map, page_map_level - 1, addr, num_4kpages);
+          SetupPageMap(child_map, page_map_level - 1, addr, num_4kpages, writable);
         if(err) {
           return {num_4kpages, err};
         }
@@ -103,16 +105,63 @@ namespace {
         }
       }
 
-      const auto entry_addr = reinterpret_cast<uintptr_t>(entry.Pointer());
-      const FrameID map_frame{entry_addr / kBytesPerFrame};
-      if(auto err = memory_manager->Free(map_frame, 1)) return err;
-
+      if(entry.bits.writable) {
+        const auto entry_addr = reinterpret_cast<uintptr_t>(entry.Pointer());
+        const FrameID map_frame{entry_addr / kBytesPerFrame};
+        if(auto err = memory_manager->Free(map_frame, 1)) return err;
+      }
       page_map[i].data = 0;
     }
 
     return MAKE_ERROR(Error::kSuccess);
   }
    
+  const FileMapping* FindFileMapping(const std::vector<FileMapping>& fmaps, uint64_t causal_addr) {
+    for(const FileMapping& m : fmaps) {
+      if(m.vaddr_begin <= causal_addr && causal_addr < m.vaddr_end) {
+        return &m;
+      }
+    }
+    return nullptr;
+  }
+
+  Error PreparePageCache(FileDescriptor& fd, const FileMapping& m, uint64_t causal_addr) {
+    LinearAddress4Level page_vaddr{causal_addr};
+    page_vaddr.parts.offset = 0;
+    if(auto err = SetupPageMaps(page_vaddr, 1)) { /* page allocation */
+      return err;
+    }
+
+    const long file_offset = page_vaddr.value - m.vaddr_begin;
+    void* page_cache = reinterpret_cast<void*>(page_vaddr.value);
+    fd.Load(page_cache, 4096, file_offset);
+    return MAKE_ERROR(Error::kSuccess);
+  }
+
+  Error SetPageContent(PageMapEntry* table, int part, LinearAddress4Level addr, PageMapEntry* content) {
+    if(part == 1) {
+      const auto i = addr.Part(part); /* get PT index which is equivalent to address*/
+      table[i].SetPointer(content);
+      table[i].bits.writable = 1;
+      InvalidateTLB(addr.value);
+      return MAKE_ERROR(Error::kSuccess);
+    }
+
+    const auto i = addr.Part(part);
+    return SetPageContent(table[i].Pointer(), part - 1, addr, content);
+  }
+
+  Error CopyOnePage(uint64_t causal_addr) {
+    auto [ p, err ] = NewPageMap();
+    if(err) {
+      return err;
+    }
+
+    const auto aligned_addr = causal_addr & 0xffff'ffff'ffff'f000;
+    memcpy(p, reinterpret_cast<const void*>(aligned_addr), 4096);
+    return SetPageContent(reinterpret_cast<PageMapEntry*>(GetCR3()), 4, LinearAddress4Level{causal_addr}, p);
+  }
+ 
 }
 
 WithError<PageMapEntry*> NewPageMap() {
@@ -131,9 +180,9 @@ Error FreePageMap(PageMapEntry* table) {
   return memory_manager->Free(frame, 1);
 }
 
-Error SetupPageMaps(LinearAddress4Level addr, size_t num_4kpages) { /* addr means Load Segment header address */
+Error SetupPageMaps(LinearAddress4Level addr, size_t num_4kpages, bool writable) { /* addr means Load Segment header address */
   auto pml4_table = reinterpret_cast<PageMapEntry*>(GetCR3()); /* CR3 register stores PML4 (highest) physical address */
-  return SetupPageMap(pml4_table, 4, addr, num_4kpages).error;
+  return SetupPageMap(pml4_table, 4, addr, num_4kpages, writable).error;
 }
 
 Error CleanPageMaps(LinearAddress4Level addr) {
@@ -141,31 +190,47 @@ Error CleanPageMaps(LinearAddress4Level addr) {
   return CleanPageMap(pml4_table, 4, addr);
 }
 
-const FileMapping* FindFileMapping(const std::vector<FileMapping>& fmaps, uint64_t causal_addr) {
-  for(const FileMapping& m : fmaps) {
-    if(m.vaddr_begin <= causal_addr && causal_addr < m.vaddr_end) {
-      return &m;
+Error CopyPageMaps(PageMapEntry* dest, PageMapEntry* src, int part, int start) {
+  if(part == 1) { /* do not copy the physical frame specified by PT */
+    for(int i = start; i < 512; i++) {
+      if(!src[i].bits.present) {
+        continue;
+      }
+      dest[i] = src[i];
+      dest[i].bits.writable = 0;
+    }
+    return MAKE_ERROR(Error::kSuccess);
+  }
+
+  for(int i = start; i < 512; i++) {
+    if(!src[i].bits.present) {
+      continue;
+    }
+
+    auto [ table, err ] = NewPageMap();
+    if(err) {
+      return err;
+    }
+
+    dest[i] = src[i];
+    dest[i].SetPointer(table);
+    if(auto err = CopyPageMaps(table, src[i].Pointer(), part - 1, 0)) {
+      return err;
     }
   }
-  return nullptr;
-}
-
-Error PreparePageCache(FileDescriptor& fd, const FileMapping& m, uint64_t causal_addr) {
-  LinearAddress4Level page_vaddr{causal_addr};
-  page_vaddr.parts.offset = 0;
-  if(auto err = SetupPageMaps(page_vaddr, 1)) { /* page allocation */
-    return err;
-  }
-
-  const long file_offset = page_vaddr.value - m.vaddr_begin;
-  void* page_cache = reinterpret_cast<void*>(page_vaddr.value);
-  fd.Load(page_cache, 4096, file_offset);
   return MAKE_ERROR(Error::kSuccess);
 }
 
 Error HandlePageFault(uint64_t error_code, uint64_t causal_addr) {
   auto& task = task_manager->CurrentTask();
-  if(error_code & 1) { // P = 1 & page level fault
+  const bool present = (error_code >> 0) & 1;
+  const bool rw = (error_code >> 1) & 1;
+  const bool user = (error_code >> 2) & 1;
+
+  if(present && rw && user) {
+    /* page fault due to readonly violation*/
+    return CopyOnePage(causal_addr);
+  } else if(present) {
     return MAKE_ERROR(Error::kAlreadyAllocated);
   }
 
