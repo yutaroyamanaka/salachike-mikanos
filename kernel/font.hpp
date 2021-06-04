60a38d1c4dc55f1414d293878c9ef499bc73831d
yutaroyamanaka <falcon4842@gmail.com>
2021-06-04 12:11:10 +0900

    day28a utf-8 intorduction

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -2,5 +2,9 @@
 #include <cstdint>
 #include "graphics.hpp"
 
+std::pair<char32_t, int> ConvertUTF8To32(const char* u8);
+bool IsHankaku(char32_t c);
+int CountUTF8Size(uint8_t c);
 void WriteAscii(PixelWriter& writer, Vector2D<int> pos, char c, const PixelColor& color);
+void WriteUnicode(PixelWriter& writer, Vector2D<int> pos, char32_t c, const PixelColor& color);
 void WriteString(PixelWriter& writer, Vector2D<int> pos, const char* s, const PixelColor& color);
